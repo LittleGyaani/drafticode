fb5aba595d393461d4552f734d8b9cdd64611165
LittleGyaani <bmohanty@live.com>
2020-05-31 16:11:29 +0530

    Fixed menu arrow and font issues

--- app/assets/templates/template-footer.php
+++ app/assets/templates/template-footer.php
@@ -127,13 +127,14 @@
   </div><!-- /.Footer-bottom -->
 </footer><!-- /.Footer -->
 
-<div class="module__search-container">
+<!-- <div class="module__search-container">
   <i class="fa fa-times close-search"></i>
   <form class="module__search-form">
     <input type="text" class="search__input" placeholder="Type Words Then Enter">
     <button class="module__search-btn"><i class="fa fa-search"></i></button>
   </form>
-</div><!-- /.module-search-container -->
+</div> -->
+<!-- /.module-search-container -->
 
 <button id="scrollTopBtn"><i class="fa fa-chevron-up"></i></button>
 </div><!-- /.wrapper -->
