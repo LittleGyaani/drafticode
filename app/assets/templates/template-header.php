c5687d07e236c155e245357486c1e0ba3463c379
LittleGyaani <bmohanty@live.com>
2020-06-15 21:10:17 +0530

    Favicon added

--- app/assets/templates/template-header.php
+++ app/assets/templates/template-header.php
@@ -1,5 +1,5 @@
 <!-- Header Section Starts Here -->
-<link href="<?= $base_URI; ?>/app/assets/images/favicon/DC-Logo.png" rel="icon">
+<link href="<?= $base_URI; ?>/app/assets/images/favicon/dc-favicon.png" rel="icon">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,500,700%7cPoppins:400,600,700&display=swap">
 <link rel="stylesheet" href="<?= $base_URI; ?>/app/assets/css/core/libraries.css" />
 <link rel="stylesheet" href="<?= $base_URI; ?>/app/assets/css/core/style.css" />
