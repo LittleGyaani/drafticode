a4845367b3c41190f347d8ff859071dad7ac722c
LittleGyaani <bmohanty@live.com>
2020-10-29 10:27:23 +0530

    Meta Details added

--- app/assets/templates/template-meta.php
+++ app/assets/templates/template-meta.php
@@ -1,6 +1,97 @@
 <!-- Meta Section Starts Here -->
+<!-- Meta updated by Little Gyaani - twitter.com/LittleGyaani -->
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-<meta name="description" content=" Drafticode Templatei">
+<meta name="description" content="Drafticode is a top notch Digital Marketing Company, Software Development & Web Development Agency in India." />
+<meta name="keywords" content="website design company bhubaneswar, web-design company in bhubaneswar, web-design company bhubaneswar, best web design company bhubaneswar, top website design company bhubaneswar, web-design company odisha, ecommerce web design company bhubaneswar, website designing services, leading web designing company bhubaneswar, professional web design company bhubaneswar, web-development company bhubaneswar, web-development company in bhubaneswar, best web development company in bhubaneswar, top web-development company in bhubaneswar, leading web development company bhubaneswar, web-application development company bhubaneswar, seo company bhubaneswar, seo services bhubaneswar, best seo company bhubaneswar, bulk sms odisha, bulk sms service bhubaneswar, web-content writing company bhubaneswar" />
+<meta name="viewport" content="width=device-width, initial-scale=1 maximum-scale=1">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="author" content="LittleGyaani - BRAHMA" />
+<meta name="topic" content="digital marketing, seo, smo, web hosting, sms marketing, bulk sms, domain names, mobile application development, android app, ios app, software development, react js, angular js, wordpress, cms, marketing agency" />
+<meta name="Search_Engines" content="Google, MSN, Bing, Overture, AltaVista, Yahoo, AOL, Infoseek, LookSmart, Excite, Hotbot, Lycos, Magellan, CNET, DogPile, Ask Jeeves, Teoma, Snap, Webcrawler" />
+<meta name='robots' content="INDEX, FOLLOW" />
+<meta name="YahooSeeker" content="INDEX, FOLLOW" />
+<meta name="msnbot" content="INDEX, FOLLOW" />
+<meta name="googlebot" content="INDEX, FOLLOW" />
+<meta name="copyright" content="Copyright (C) 2019-2020 Drafticode, A Kaisileum LABS Group Company." />
+<meta name="owner" content="bmohanty@live.com"  />
+<meta name="allow-search" content="yes"  />
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta http-equiv="cache-control" content="max-age=0" />
+<meta http-equiv="expires" content="0" />
+<meta http-equiv="cache-control" content="no-cache, no-store, must-revalidate" />
+<meta http-equiv="Expires" content="0" />
+<meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+<meta http-equiv="pragma" content="no-cache" />
+<meta name="rating" content="General">
+<meta name="audience" content="All">
+<meta name="doc-type" content="Web Page">
+<meta name="doc-class" content="Published">
+
+<!-- Cannonical Link -->
+<link rel="canonical" href="https://drafticode.com/" />
+
+<!-- Open Graph / Facebook -->
+<meta property="og:locale" content="en_IN" />
+<meta property='og:title' content="Digital Marketing Agency, Web & App Development Company in India : Drafticode" />
+<meta property='og:description' content="Drafticode is a top notch Digital Marketing Company, Software Development & Web Development Agency in India." />
+<meta property='og:type' content="Website" />
+<meta property='og:url' content="https://drafticode.com/" />
+<meta property="og:image" content="https://drafticode.com/app/assets/images/favicon/dc-favicon.png" />
+<meta property='og:image:width' content="256" />
+<meta property='og:image:height' content="256" />
+<meta property='og:image:type' content="image/png" />
+
+<!-- Twitter -->
+<meta property="twitter:card" content="Drafticode"/>
+<meta property="twitter:url" content="https://drafticode.com/"/>
+<meta property="twitter:title" content="Digital Marketing Agency, Web & App Development Company in India : Drafticode"/>
+<meta property="twitter:description" content="Drafticode is a top notch Digital Marketing Company, Software Development & Web Development Agency in India."/>
+<meta property="twitter:image" content="https://drafticode.com/app/assets/images/favicon/dc-favicon.png"/>
+
+<!-- LD JSON SCHEMA -->
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "LocalBusiness",
+  "name": "Drafticode",
+  "image": "https://drafticode.com/app/assets/images/favicon/dc-favicon.png",
+  "@id": "https://drafticode.com/",
+  "url": "https://drafticode.com/",
+  "telephone": "+919861779166",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "Office -2, Arihant Plaza, In-front of OMFED Office, Swash Lane, Saheed Nagar",
+    "addressLocality": "Bhubaneswar",
+    "addressRegion": "OD",
+    "postalCode": "751007",
+    "addressCountry": "IN"
+  },
+  "geo": {
+    "@type": "GeoCoordinates",
+    "latitude": 20.2492487,
+    "longitude": 85.8511827
+  },
+  "openingHoursSpecification": {
+    "@type": "OpeningHoursSpecification",
+    "dayOfWeek": [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday"
+    ],
+    "opens": "09:00",
+    "closes": "21:00"
+  },
+  "sameAs": [
+    "https://www.facebook.com/drafticode/",
+    "https://instagram.com/drafticode",
+    "https://drafticode.com/"
+  ]
+}
+</script>
  <!-- Meta Section Ends Here -->
